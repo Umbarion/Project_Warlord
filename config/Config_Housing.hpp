cef10e5180b544a2e2b3dca5c18867d193c23994
Paini <paini9819@gmail.com>
2016-11-18 12:34:37 +0100

    Housing angepasst (WIP)

--- config/Config_Housing.hpp
+++ config/Config_Housing.hpp
@@ -17,6 +17,92 @@
 */
 
 class Housing {
+	class IslaDuala3 {
+
+        class Land_i_House_Big_02_V1_F {
+            price = 1550000;
+            numberCrates = 3;
+            restrictedPos[] = {0,1,2,3,4};
+            canGarage = false;
+            garageSpawnPos[] = {};
+            garageSpawnDir = 0;
+            garageBlacklists[] = {};
+            lightPos[] = {2,0,3.5};
+        };
+        
+        class Land_i_House_Big_02_V2_F : Land_i_House_Big_02_V1_F{};
+        class Land_i_House_Big_02_V3_F : Land_i_House_Big_02_V1_F{};
+
+        class Land_i_House_Big_01_V1_F {
+            price = 2200000;
+            numberCrates = 4;
+            restrictedPos[] = {2,3};
+            canGarage = false;
+            garageSpawnPos[] = {};
+            garageSpawnDir = 0;
+            garageBlacklists[] = {};
+            lightPos[] = {0,-2,3.8};
+        };
+
+        class Land_i_House_Big_01_V2_F : Land_i_House_Big_01_V1_F{};
+        class Land_i_House_Big_01_V3_F : Land_i_House_Big_01_V1_F{};
+
+        class Land_i_House_Small_01_V1_F {
+            price = 1050000;
+            numberCrates = 2;
+            restrictedPos[] = {};
+            canGarage = false;
+            garageSpawnPos[] = {};
+            garageSpawnDir = 0;
+            garageBlacklists[] = {};
+            lightPos[] = {-1.5,0,2};
+        };
+
+        class Land_i_House_Small_01_V2_F : Land_i_House_Small_01_V1_F{};
+        class Land_i_House_Small_01_V3_F : Land_i_House_Small_01_V1_F{};
+
+        class Land_i_House_Small_02_V1_F {
+            price = 1000500;
+            numberCrates = 2;
+            restrictedPos[] = {};
+            canGarage = false;
+            garageSpawnPos[] = {};
+            garageSpawnDir = 0;
+            garageBlacklists[] = {};
+            lightPos[] = {2,0,2.4};
+        };
+
+        class Land_i_House_Small_02_V2_F : Land_i_House_Small_02_V1_F{};
+        class Land_i_House_Small_02_V3_F : Land_i_House_Small_02_V1_F{};
+
+        class Land_i_House_Small_03_V1_F {
+            price = 1250000;
+            numberCrates = 3;
+            restrictedPos[] = {};
+            canGarage = false;
+            garageSpawnPos[] = {};
+            garageSpawnDir = 0;
+            garageBlacklists[] = {};
+            lightPos[] = {-3.3,1,2.5};
+        };
+
+        class Land_i_House_Small_03_V3_F : Land_i_House_Small_03_V1_F{};
+
+        class Land_i_Stone_HouseSmall_V1_F {
+            price = 750000;
+            numberCrates = 1;
+            restrictedPos[] = {0,1,3,4};
+            canGarage = false;
+            garageSpawnPos[] = {};
+            garageSpawnDir = 0;
+            garageBlacklists[] = {};
+            lightPos[] = {0.90,2.3,4};
+        };
+
+        class Land_i_Stone_HouseSmall_V2_F : Land_i_Stone_HouseSmall_V1_F{};
+        class Land_i_Stone_HouseSmall_V3_F : Land_i_Stone_HouseSmall_V1_F{};
+
+    };
   
     class Altis {
 
