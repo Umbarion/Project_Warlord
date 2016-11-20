270af8a81f74eb139993e622e77ae607baa4a880
Paini <paini9819@gmail.com>
2016-11-20 16:28:31 +0100

    vitems entmüllt

--- config/Config_vItems.hpp
+++ config/Config_vItems.hpp
@@ -148,701 +148,386 @@ class VirtualItems {
 
     class PWA_defibrillator {
         variable = "PWA_defibrillator";
-        displayName = "STR_Item_defibrillator";
-        weight = 4;
         buyPrice = 900;
         sellPrice = 450;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_defibrillator.paa";
     };
 
     class PWA_toolkit {
         variable = "PWA_toolkit";
-        displayName = "STR_Item_Toolkit";
-        weight = 4;
         buyPrice = 350;
         sellPrice = 100;
-        illegal = false;
-        edible = -1;
-        icon = "\a3\weapons_f\items\data\UI\gear_toolkit_ca.paa";
     };
 
     class PWA_jerrycan_empty {
         variable = "PWA_jerrycan_empty";
-        displayName = "STR_Item_FuelE";
-        weight = 2;
         buyPrice = -1;
         sellPrice = 10;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_fuelEmpty.paa";
     };
 
     class PWA_jerrycan_full {
         variable = "PWA_jerrycan_full";
-        displayName = "STR_Item_FuelF";
-        weight = 5;
         buyPrice = 850;
         sellPrice = 500;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_fuel.paa";
     };
 
     class PWA_nailribbon {
         variable = "PWA_nailribbon";
-        displayName = "STR_Item_SpikeStrip";
-        weight = 15;
         buyPrice = 2500;
         sellPrice = 1200;
-        illegal = true;
-        edible = -1;
-        icon = "icons\ico_spikeStrip.paa";
     };
 
     class PWA_lockpick {
         variable = "PWA_lockpick";
-        displayName = "STR_Item_Lockpick";
-        weight = 1;
         buyPrice = 150;
         sellPrice = 75;
-        illegal = true;
-        edible = -1;
-        icon = "icons\ico_lockpick.paa";
     };
 
     class PWA_gold_processed {
         variable = "PWA_gold_processed";
-        displayName = "STR_Item_GoldBar";
-        weight = 12;
         buyPrice = -1;
         sellPrice = 95000;
-        illegal = true;
-        edible = -1;
-        icon = "icons\ico_goldBar.paa";
     };
 
     class blastingcharge {
         variable = "blastingCharge";
-        displayName = "STR_Item_BCharge";
-        weight = 15;
         buyPrice = 35000;
         sellPrice = 10000;
-        illegal = true;
-        edible = -1;
-        icon = "icons\ico_blastingCharge.paa";
     };
 
     class PWA_boltcutter {
         variable = "PWA_boltcutter";
-        displayName = "STR_Item_BCutter";
-        weight = 5;
         buyPrice = 7500;
         sellPrice = 1000;
-        illegal = true;
-        edible = -1;
-        icon = "icons\ico_boltCutter.paa";
     };
 
     class defusekit {
         variable = "defuseKit";
-        displayName = "STR_Item_DefuseKit";
-        weight = 2;
         buyPrice = 2500;
         sellPrice = 2000;
-        illegal = true;
-        edible = -1;
-        icon = "icons\ico_defuseKit.paa";
     };
 
     class PWA_storagecrate_small {
         variable = "PWA_storagecrate_small";
-        displayName = "STR_Item_StorageBS";
-        weight = 5;
         buyPrice = 75000;
         sellPrice = 50000;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_storageSmall.paa";
     };
 
     class PWA_storagecrate_big {
         variable = "PWA_storagecrate_big";
-        displayName = "STR_Item_StorageBL";
-        weight = 10;
         buyPrice = 150000;
         sellPrice = 125000;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_storageBig.paa";
     };
 
     //Mined Items
     class oil_unprocessed {
         variable = "oilUnprocessed";
-        displayName = "STR_Item_OilU";
-        weight = 7;
         buyPrice = -1;
         sellPrice = -1;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_oilUnprocessed.paa";
     };
 
     class PWA_oil_processed {
         variable = "PWA_oil_processed";
-        displayName = "STR_Item_OilP";
-        weight = 6;
         buyPrice = -1;
         sellPrice = 3200;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_oilProcessed.paa";
     };
 
     class PWA_copper_raw {
         variable = "PWA_copper_raw";
-        displayName = "STR_Item_CopperOre";
-        weight = 4;
         buyPrice = -1;
         sellPrice = -1;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_copperOre.paa";
     };
 
     class PWA_copper_processed {
         variable = "PWA_copper_processed";
-        displayName = "STR_Item_CopperIngot";
-        weight = 3;
         buyPrice = -1;
         sellPrice = 1500;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_copper.paa";
     };
 
     class PWA_iron_raw {
         variable = "PWA_iron_raw";
-        displayName = "STR_Item_IronOre";
-        weight = 5;
         buyPrice = -1;
         sellPrice = -1;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_ironOre.paa";
     };
 
     class PWA_iron_processed {
         variable = "PWA_iron_processed";
-        displayName = "STR_Item_IronIngot";
-        weight = 3;
         buyPrice = -1;
         sellPrice = 3200;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_iron.paa";
     };
 
     class salt_unrefined {
         variable = "saltUnrefined";
-        displayName = "STR_Item_Salt";
-        weight = 3;
         buyPrice = -1;
         sellPrice = -1;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_saltUnprocessed.paa";
     };
 
     class salt_refined {
         variable = "saltRefined";
-        displayName = "STR_Item_SaltR";
-        weight = 1;
         buyPrice = -1;
         sellPrice = 1450;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_saltProcessed.paa";
     };
 
     class sand {
         variable = "sand";
-        displayName = "STR_Item_Sand";
-        weight = 3;
         buyPrice = -1;
         sellPrice = -1;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_sand.paa";
     };
 
     class glass {
         variable = "glass";
-        displayName = "STR_Item_Glass";
-        weight = 1;
         buyPrice = -1;
         sellPrice = 1450;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_glass.paa";
     };
 
     class PWA_diamond_raw {
         variable = "diamondUncut";
-        displayName = "STR_Item_DiamondU";
-        weight = 4;
         buyPrice = -1;
         sellPrice = 750;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_diamondUncut.paa";
     };
 
     class PWA_diamond_processed {
         variable = "diamondCut";
-        displayName = "STR_Item_DiamondC";
-        weight = 2;
         buyPrice = -1;
         sellPrice = 2000;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_diamondCut.paa";
     };
 
     class PWA_stone_raw {
         variable = "PWA_stone_raw";
-        displayName = "STR_Item_Rock";
-        weight = 6;
         buyPrice = -1;
         sellPrice = -1;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_rock.paa";
     };
 
     class PWA_stone_processed {
         variable = "PWA_stone_processed";
-        displayName = "STR_Item_CementBag";
-        weight = 5;
         buyPrice = -1;
         sellPrice = 1950;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_cement.paa";
     };
 
     //Drugs
     class PWA_heroin_raw {
         variable = "PWA_heroin_raw";
-        displayName = "STR_Item_HeroinU";
-        weight = 6;
         buyPrice = -1;
         sellPrice = -1;
-        illegal = true;
-        edible = -1;
-        icon = "icons\ico_heroinUnprocessed.paa";
         processedItem = "PWA_heroin_processed";
     };
 
     class PWA_heroin_processed {
         variable = "PWA_heroin_processed";
-        displayName = "STR_Item_HeroinP";
-        weight = 4;
         buyPrice = 3500;
         sellPrice = 2560;
-        illegal = true;
-        edible = -1;
-        icon = "icons\ico_heroinProcessed.paa";
     };
 
     class PWA_cannabis_raw {
         variable = "PWA_cannabis_raw";
-        displayName = "STR_Item_Cannabis";
-        weight = 4;
         buyPrice = -1;
         sellPrice = -1;
-        illegal = true;
-        edible = -1;
-        icon = "icons\ico_cannabis.paa";
         processedItem = "PWA_cannabis_processed";
     };
 
     class PWA_cannabis_processed {
         variable = "PWA_cannabis_processed";
-        displayName = "STR_Item_Marijuana";
-        weight = 3;
         buyPrice = 2800;
         sellPrice = 2350;
-        illegal = true;
-        edible = -1;
-        icon = "icons\ico_marijuana.paa";
     };
 
     class PWA_cocaine_raw {
         variable = "PWA_cocaine_raw";
-        displayName = "STR_Item_CocaineU";
-        weight = 6;
         buyPrice = -1;
         sellPrice = -1;
-        illegal = true;
-        edible = -1;
-        icon = "icons\ico_cocaineUnprocessed.paa";
         processedItem = "PWA_cocaine_processed";
     };
 
     class PWA_cocaine_processed {
         variable = "PWA_cocaine_processed";
-        displayName = "STR_Item_CocaineP";
-        weight = 4;
         buyPrice = -1;
         sellPrice = 5000;
-        illegal = true;
-        edible = -1;
-        icon = "icons\ico_cocaineProcessed.paa";
     };
 
     //Drink
     class PWA_energydrink {
         variable = "PWA_energydrink";
-        displayName = "STR_Item_RedGull";
-        weight = 1;
         buyPrice = 1500;
         sellPrice = 200;
-        illegal = false;
-        edible = 100;
-        icon = "icons\ico_redgull.paa";
     };
 
     class PWA_coffee {
         variable = "PWA_coffee";
-        displayName = "STR_Item_Coffee";
-        weight = 1;
         buyPrice = 10;
         sellPrice = 5;
-        illegal = false;
-        edible = 100;
-        icon = "icons\ico_coffee.paa";
     };
 
     class PWA_waterbottle {
         variable = "PWA_waterbottle";
-        displayName = "STR_Item_WaterBottle";
-        weight = 1;
         buyPrice = 10;
         sellPrice = 5;
-        illegal = false;
-        edible = 100;
-        icon = "icons\ico_waterBottle.paa";
     };
 
     //Food
     class apple {
         variable = "apple";
-        displayName = "STR_Item_Apple";
-        weight = 1;
         buyPrice = 65;
         sellPrice = 50;
-        illegal = false;
-        edible = 10;
-        icon = "icons\ico_apple.paa";
     };
 
     class peach {
         variable = "peach";
-        displayName = "STR_Item_Peach";
-        weight = 1;
         buyPrice = 68;
         sellPrice = 55;
-        illegal = false;
-        edible = 10;
-        icon = "icons\ico_peach.paa";
     };
 
     class PWA_beefjerky {
         variable = "PWA_beefjerky";
-        displayName = "STR_Item_TBacon";
-        weight = 1;
         buyPrice = 75;
         sellPrice = 25;
-        illegal = false;
-        edible = 40;
-        icon = "icons\ico_tBacon.paa";
     };
 
     class PWA_donut {
         variable = "PWA_donut";
-        displayName = "STR_Item_Donuts";
-        weight = 1;
         buyPrice = 120;
         sellPrice = 60;
-        illegal = false;
-        edible = 30;
-        icon = "icons\ico_donuts.paa";
     };
 
     class rabbit_raw {
         variable = "rabbitRaw";
-        displayName = "STR_Item_RabbitRaw";
-        weight = 2;
         buyPrice = -1;
         sellPrice = 95;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_rabbitRaw.paa";
     };
 
     class rabbit {
         variable = "rabbit";
-        displayName = "STR_Item_Rabbit";
-        weight = 1;
         buyPrice = 150;
         sellPrice = 115;
-        illegal = false;
-        edible = 20;
-        icon = "icons\ico_rabbit.paa";
     };
 
     class PWA_salema_raw{
         variable = "PWA_salema_raw";
-        displayName = "STR_Item_SalemaRaw";
-        weight = 2;
         buyPrice = -1;
         sellPrice = 45;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_salemaRaw.paa";
     };
 
     class salema {
         variable = "salema";
-        displayName = "STR_Item_Salema";
-        weight = 1;
         buyPrice = 75;
         sellPrice = 55;
-        illegal = false;
-        edible = 30;
-        icon = "icons\ico_cookedFish.paa";
     };
 
     class PWA_ornate_raw {
         variable = "PWA_ornate_raw";
-        displayName = "STR_Item_OrnateRaw";
-        weight = 2;
         buyPrice = -1;
         sellPrice = 40;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_ornateRaw.paa";
     };
 
     class ornate {
         variable = "ornate";
-        displayName = "STR_Item_Ornate";
-        weight = 1;
         buyPrice = 175;
         sellPrice = 150;
-        illegal = false;
-        edible = 25;
-        icon = "icons\ico_cookedFish.paa";
     };
 
     class PWA_mackerel_raw {
         variable = "PWA_mackerel_raw";
-        displayName = "STR_Item_MackerelRaw";
-        weight = 4;
         buyPrice = -1;
         sellPrice = 175;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_mackerelRaw.paa";
     };
 
     class mackerel {
         variable = "mackerel";
-        displayName = "STR_Item_Mackerel";
-        weight = 2;
         buyPrice = 250;
         sellPrice = 200;
-        illegal = false;
-        edible = 30;
-        icon = "icons\ico_cookedFish.paa";
     };
 
     class PWA_tuna_raw {
         variable = "PWA_tuna_raw";
-        displayName = "STR_Item_TunaRaw";
-        weight = 6;
         buyPrice = -1;
         sellPrice = 700;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_tunaRaw.paa";
     };
 
     class tuna {
         variable = "tuna";
-        displayName = "STR_Item_Tuna";
-        weight = 3;
         buyPrice = 1250;
         sellPrice = 1000;
-        illegal = false;
-        edible = 100;
-        icon = "icons\ico_cookedFish.paa";
     };
 
     class PWA_mullet_raw {
         variable = "PWA_mullet_raw";
-        displayName = "STR_Item_MulletRaw";
-        weight = 4;
         buyPrice = -1;
         sellPrice = 250;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_mulletRaw.paa";
     };
 
     class mullet {
         variable = "mullet";
-        displayName = "STR_Item_Mullet";
-        weight = 2;
         buyPrice = 600;
         sellPrice = 400;
-        illegal = false;
-        edible = 80;
-        icon = "icons\ico_cookedFish.paa";
     };
 
     class PWA_catshark_raw {
         variable = "PWA_catshark_raw";
-        displayName = "STR_Item_CatSharkRaw";
-        weight = 6;
         buyPrice = -1;
         sellPrice = 300;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_catsharkRaw.paa";
     };
 
     class catshark {
         variable = "catshark";
-        displayName = "STR_Item_CatShark";
-        weight = 3;
         buyPrice = 750;
         sellPrice = 500;
-        illegal = false;
-        edible = 100;
-        icon = "icons\ico_cookedFish.paa";
     };
 
     class turtle_raw {
         variable = "turtleRaw";
-        displayName = "STR_Item_TurtleRaw";
-        weight = 6;
         buyPrice = -1;
         sellPrice = 3000;
-        illegal = true;
-        edible = -1;
-        icon = "icons\ico_turtleRaw.paa";
     };
 
     class turtle_soup {
         variable = "turtleSoup";
-        displayName = "STR_Item_TurtleSoup";
-        weight = 2;
         buyPrice = 1000;
         sellPrice = 750;
-        illegal = false;
-        edible = 100;
-        icon = "icons\ico_turtleSoup.paa";
     };
 
     class hen_raw {
         variable = "henRaw";
-        displayName = "STR_Item_HenRaw";
-        weight = 1;
         buyPrice = -1;
         sellPrice = 65;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_wholeChickenRaw.paa";
     };
 
     class hen {
         variable = "hen";
-        displayName = "STR_Item_Hen";
-        weight = 1;
         buyPrice = 115;
         sellPrice = 85;
-        illegal = false;
-        edible = 65;
-        icon = "icons\ico_wholeChicken.paa";
     };
 
     class rooster_raw {
         variable = "roosterRaw";
-        displayName = "STR_Item_RoosterRaw";
-        weight = 1;
         buyPrice = -1;
         sellPrice = 65;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_chickenDrumstickRaw.paa";
     };
 
     class rooster {
         variable = "rooster";
-        displayName = "STR_Item_Rooster";
-        weight = 115;
         buyPrice = 90;
         sellPrice = 85;
-        illegal = false;
-        edible = 45;
-        icon = "icons\ico_chickenDrumstick.paa";
     };
 
     class sheep_raw {
         variable = "sheepRaw";
-        displayName = "STR_Item_SheepRaw";
-        weight = 2;
         buyPrice = -1;
         sellPrice = 95;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_lambChopRaw.paa";
     };
 
     class sheep {
         variable = "sheep";
-        displayName = "STR_Item_Sheep";
-        weight = 2;
         buyPrice = 155;
         sellPrice = 115;
-        illegal = false;
-        edible = 100;
-        icon = "icons\ico_lambChop.paa";
     };
 
     class goat_raw {
         variable = "goatRaw";
-        displayName = "STR_Item_GoatRaw";
-        weight = 2;
         buyPrice = -1;
         sellPrice = 115;
-        illegal = false;
-        edible = -1;
-        icon = "icons\ico_muttonLegRaw.paa";
     };
 
     class goat {
         variable = "goat";
-        displayName = "STR_Item_Goat";
-        weight = 2;
         buyPrice = 175;
         sellPrice = 135;
-        illegal = false;
-        edible = 100;
-        icon = "icons\ico_muttonLeg.paa";
     };
 };
